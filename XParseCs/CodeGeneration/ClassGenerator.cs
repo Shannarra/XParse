7ffe1a9581a3021f3e44c2854ac4a1362e1c689a
Shannarra <petarangelov15@gmail.com>
2020-04-15 16:36:17 +0300

    Added complete class and constructor generation

--- XParseCs/CodeGeneration/ClassGenerator.cs
+++ XParseCs/CodeGeneration/ClassGenerator.cs
@@ -2,21 +2,44 @@
 
 namespace XParseCs.CodeGeneration
 {
+
     class ClassGenerator : ICodeGenerator
     {
+        /// <summary>
+        /// The class' name
+        /// </summary>
         readonly string name;
 
+        /// <summary>
+        /// The class' protection level
+        /// </summary>
         readonly string? protectionLevel;
 
+        /// <summary>
+        /// Is this class partial?
+        /// </summary>
         readonly bool? isPartial;
 
+        /// <summary>
+        /// Is this class unsafe?
+        /// </summary>
         readonly bool? isUnsafe;
 
-        readonly string[]? others;
+        /// <summary>
+        /// Any additional modifiers, such as "static", "sealed", et cetera
+        /// </summary>
+        readonly string[]? additional;
 
+        /// <summary>
+        /// The class' summary, if it has one
+        /// </summary>
         readonly string? summary;
 
+        /// <summary>
+        /// The list of inheritance : IInterface, BaseClass
+        /// </summary>
         readonly string[]? inherits;
+
 
         public ClassGenerator(Attributes attributes)
         {
@@ -24,11 +47,35 @@ namespace XParseCs.CodeGeneration
                 throw new XParseAttributeException("name");
             else
                 name = attributes["name"].Value;
+
+            if (attributes["protection"] != null) // protection level
+                protectionLevel = attributes["protection"].Value;
+
+            if (attributes["isPartial"] != null) // partial?
+                isPartial = attributes["isPartial"].Value == "true";
+
+            if (attributes["isUnsafe"] != null) // unsafe?
+                isUnsafe = attributes["isUnsafe"].Value == "true";
+
+            if (attributes["additional"] != null) // splitting the 'additional' list
+                additional = attributes["additional"].Value.Contains(',') ? attributes["additional"].Value.Split(",", 
+                    System.StringSplitOptions.RemoveEmptyEntries) : new string[] { attributes["additional"].Value };
+
+            if (attributes["summary"] != null) // getting the summary
+                summary = attributes["summary"].Value;
+
+            if (attributes["inherits"] != null) // splitting the 'inherits' list
+                inherits = attributes["inherits"].Value.Contains(',') ? attributes["inherits"].Value.Split(',',
+                    System.StringSplitOptions.RemoveEmptyEntries) : new string[] { attributes["inherits"].Value };
         }
 
         public string Parse()
-        {
-            return "";
-        }
+            => $"\n\t" +
+                $"{(summary == null ? string.Empty : $"///<summary>\n\t/// {summary}\n\t///</summary>\n\t")}" +
+                $"{(protectionLevel == null ? string.Empty : protectionLevel + " ")}" +
+                $"{(additional == null ? string.Empty : additional.Reduce() + " ")}" +
+                $"{(isUnsafe == null ? string.Empty : isUnsafe.Value ? "unsafe " : string.Empty)}" +
+                $"{(isPartial == null ? string.Empty : isPartial.Value ? "partial " : string.Empty)}" +
+                $"class {name} {(inherits == null ? "\n\t{" : ": " + inherits.Reduce(',') + "\n\t{")}";
     }
 }
--- XParseCs/ParseExtensions.cs
+++ XParseCs/ParseExtensions.cs
@@ -6,6 +6,8 @@
 
         public static readonly string TAB = "\t";
 
+        public static string CurrentClass;
+
         public static string? ParseTrigger(System.Xml.XmlNode root)
         {
             string accumulatedText = "";
@@ -17,8 +19,8 @@
 
             if (lastNodeVisited == "namespace") // start a #region
                 accumulatedText = $"{TAB}#region Dependencies";
-            if (root.Name != lastNodeVisited && lastNodeVisited == "ref") // we went trough the last "using" directive
-                accumulatedText = $"{TAB}#endregion Dependencies";
+            if (root.Name == "class") // all "using" directives MUST be b4 class' start.
+                accumulatedText = $"{TAB}#endregion Dependencies\n";
 
             if (root.Name == "ref")
                 accumulatedText = new CodeGeneration.DepedenciesGenerator(
@@ -27,12 +29,37 @@
                     root.Attributes["aliasName"]).Parse();
 
             if (root.Name == "class")
-                accumulatedText = new CodeGeneration.ClassGenerator(root.Attributes).Parse();
+            {
+                accumulatedText += new CodeGeneration.ClassGenerator(root.Attributes).Parse();
+                CurrentClass = root.Attributes["name"].Value;
+            }
 
+            if (root.Name == "ctor")
+            {
+                //adding a summary by default for the constructor
+                accumulatedText = $"\n\t\t///<summary>\n\t\t/// Creates a new {CurrentClass} object\n\t\t///</summarry>\n";
+                if (root.Attributes.Count > 0 && !string.IsNullOrEmpty(root.Attributes["params"].Value))
+                    accumulatedText += $"{TAB}{TAB}{CurrentClass}" +
+                        $"({(root.Attributes["params"].Value.Contains(',') ? root.Attributes["params"].Value.Split(',').Reduce(',') : root.Attributes["params"].Value)})\n\t\t" + "{\n\t\t\t//initializing object of type " + CurrentClass + " here\n\t\t}";
+                else accumulatedText += $"{TAB}{TAB}{CurrentClass}()\n\t\t" + "{\n\t\t\t//initializing object of type " + CurrentClass + " here\n\t\t}"; // no parameters
+            }
 
             lastNodeVisited = root.Name;
 
-            return accumulatedText; // not found or recognized?
+            return accumulatedText;
+        }
+
+        public static string Reduce(this string[] arr, char join = ' ')
+        {
+            string str = "";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += arr[i];
+                if (i < arr.Length - 1)
+                    str += join;
+            }
+            return str;
         }
     }
 }
